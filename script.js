291dc0efc1aa5d08dccadcd34ffc60b909bf1c2a
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-22 19:23:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ function removeSelectedColor() {
+      // Get the dropdown element
+      var dropdown = document.getElementById("colorSelect");
+
+      // Get the selected index
+      var selectedIndex = dropdown.selectedIndex;
+
+      // Remove the selected option
+      if (selectedIndex !== -1) {
+        dropdown.remove(selectedIndex);
+      }
+    }
